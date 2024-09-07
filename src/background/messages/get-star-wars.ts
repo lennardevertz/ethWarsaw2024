9ba4dbeeae8872bcbec2359bdb473aa072584a54
nemmtor <kacper0witas@gmail.com>
2024-09-07 12:50:32 +0200

    chore: initial setup

---
+++ src/background/messages/get-star-wars.ts
@@ -0,0 +1,10 @@
+import type { PlasmoMessaging } from "@plasmohq/messaging"
+
+const handler: PlasmoMessaging.MessageHandler = async (_req, res) => {
+  const swapiResponse = await fetch("https://swapi.dev/api/people/1")
+  const responseBody = await swapiResponse.json()
+
+  res.send(responseBody)
+}
+
+export default handler
---
+++ src/content.tsx
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query"
+import cssText from "data-text:~style.css"
+
+import { sendToBackground } from "@plasmohq/messaging"
+
+import { withProviders } from "~providers"
+
+export const getStyle = () => {
+  const style = document.createElement("style")
+  console.log(style)
+  style.textContent = cssText
+  return style
+}
+
+const ContentScript = () => {
+  const getStarWarsQuery = useQuery({
+    queryKey: ["get-star-wars"],
+    queryFn: () => {
+      return sendToBackground({
+        name: "get-star-wars"
+      })
+    }
+  })
+
+  return (
+    <div className="absolute top-20 left-20 bg-green-300 text-black">
+      {JSON.stringify(getStarWarsQuery.data)}
+    </div>
+  )
+}
+
+export default withProviders(ContentScript)
