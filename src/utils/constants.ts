7746607b4e6ca03ecdd3395430ae072a045b7466
lennardevertz <levertz@idriss.xyz>
2024-09-07 17:27:06 +0200

    move optional params

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -27,19 +27,19 @@ export const getTokenIconUrl = (
 
 /**
  * 
- * @param inputToken Token used for buying
  * @param outputToken Ticker of token user wants to buy
- * @param inputNetwork Network the user buys from
  * @param outputNetwork Target network, == inputNetwork for simple token swap action
  * @param inputAmount absolute amount you want to buy -> example 0.001 (if you want to buy 0.001ETH worth of outputToken)
+ * @param inputToken Token used for buying
+ * @param inputNetwork Network the user buys from
  * @returns Prompt that can be used in resolve-brian
  */
 export const getBrianPrompt = (
-  inputToken: string = "ETH",
   outputToken: string,
-  inputNetwork: keyof typeof UNISWAP_V3_SUBGRAPH_ID = "BASE",
   outputNetwork: keyof typeof UNISWAP_V3_SUBGRAPH_ID,
-  inputAmount: number
+  inputAmount: number,
+  inputToken: string = "ETH",
+  inputNetwork: keyof typeof UNISWAP_V3_SUBGRAPH_ID = "BASE"
 ) => {
   if (inputNetwork===outputNetwork) {
     return `Swap ${inputAmount} of ${inputToken} to ${outputToken} on ${inputNetwork}`
