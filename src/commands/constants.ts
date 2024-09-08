387e451d58eb96826c9d18368db571d7ed3edf16
nemmtor <kacper0witas@gmail.com>
2024-09-08 02:47:06 +0200

    fix: conflicts

--- src/commands/constants.ts
+++ src/commands/constants.ts
@@ -14,6 +14,20 @@ export const UNISWAP_V3_SUBGRAPH_ID = {
   ARBITRUM: 'FbCGRftH4a3yZugY7TnbYgPJVEv2LvMT6oF1fxPe9aJM',
 } as const;
 
+export const TWITTER_TO_ETH = {
+  vitalikbuterin: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
+  dwr: '0xD7029BDEa1c17493893AAfE29AAD69EF892B8ff2',
+  geoist_: '0xcCE9A28b570946123f392Cf1DbfA6D2D5e636a1f',
+  levertz_: '0x4a3755eB99ae8b22AaFB8f16F0C51CF68Eb60b85',
+} as const;
+
+export const WARPCAST_TO_ETH = {
+  'vitalik.eth': '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
+  'dwr': '0xD7029BDEa1c17493893AAfE29AAD69EF892B8ff2',
+  'geoist': '0xcCE9A28b570946123f392Cf1DbfA6D2D5e636a1f',
+  'levertz': '0x4a3755eB99ae8b22AaFB8f16F0C51CF68Eb60b85',
+} as const;
+
 export const ENS_TO_TWITTER: Record<string, string> = {
   'vitalik.eth': 'vitalikbuterin',
   'dwr.eth': 'dwr',
