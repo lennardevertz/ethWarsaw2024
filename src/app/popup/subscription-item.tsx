3b13248fc98535689dc72347ce8d41b1ca73989b
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-09-08 08:27:17 +0200

    added ens to text icons

--- src/app/popup/subscription-item.tsx
+++ src/app/popup/subscription-item.tsx
@@ -1,8 +1,22 @@
 import { useCallback } from 'react';
 
-import { anonymousUserAvatar, twitterLogo, warpcastLogo } from 'images';
+import {
+  anonymousUserAvatar,
+  twitterLogo,
+  warpcastLogo,
+  DISCORD_ICON,
+  EMAIL_ICON,
+  GITHUB_ICON,
+} from 'images';
 import { Subscription } from 'types';
 import { IconButton } from 'components';
+import {
+  GetEnsDiscordCommand,
+  GetEnsEmailCommand,
+  GetEnsGithubCommand,
+  GetEnsTwitterCommand,
+  useCommandQuery,
+} from 'commands';
 
 type Props = {
   subscription: Subscription;
@@ -13,6 +27,26 @@ export const SubscriptionItem = ({ subscription, onRemove }: Props) => {
   const remove = useCallback(() => {
     onRemove(subscription);
   }, [onRemove, subscription]);
+
+  const emailQuery = useCommandQuery({
+    command: new GetEnsEmailCommand({ ensName: subscription.ensName }),
+    staleTime: Number.POSITIVE_INFINITY,
+  });
+
+  const twitterQuery = useCommandQuery({
+    command: new GetEnsTwitterCommand({ ensName: subscription.ensName }),
+    staleTime: Number.POSITIVE_INFINITY,
+  });
+
+  const githubQuery = useCommandQuery({
+    command: new GetEnsGithubCommand({ ensName: subscription.ensName }),
+    staleTime: Number.POSITIVE_INFINITY,
+  });
+
+  const discordQuery = useCommandQuery({
+    command: new GetEnsDiscordCommand({ ensName: subscription.ensName }),
+    staleTime: Number.POSITIVE_INFINITY,
+  });
 
   return (
     <li className="flex items-center justify-between">
@@ -31,6 +65,39 @@ export const SubscriptionItem = ({ subscription, onRemove }: Props) => {
               rel="noreferrer noopener"
             >
               <img src={twitterLogo} alt="Twitter" className="size-4" />
+            </a>
+          )}
+          {subscription.twitterUsername ||
+            (twitterQuery.data && (
+              <a
+                href={`https://x.com/${subscription.twitterUsername ?? twitterQuery.data}`}
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <img src={twitterLogo} alt="Twitter" className="size-4" />
+              </a>
+            ))}
+          {githubQuery.data && (
+            <a
+              href={`https://github.com/${githubQuery.data}`}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img src={GITHUB_ICON} alt="GitHub" className="size-4" />
+            </a>
+          )}
+          {discordQuery.data && (
+            <span title={discordQuery.data}>
+              <img src={DISCORD_ICON} alt="GitHub" className="size-4" />
+            </span>
+          )}
+          {emailQuery.data && (
+            <a
+              href={`mailto:${emailQuery.data}`}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img src={EMAIL_ICON} alt="GitHub" className="size-4" />
             </a>
           )}
           {subscription.farcasterUsername && (
